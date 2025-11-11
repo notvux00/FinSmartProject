87391624fa64204b954b2af40bdef20929d9a300
Lê Duy Vũ <djanh123456@gmail.com>
2025-11-11 20:07:53 +0700

    test

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import AppRouter from "./frontend/AppRouter";
+import App from "./app/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AppRouter />
+    <App />
   </React.StrictMode>
 );
